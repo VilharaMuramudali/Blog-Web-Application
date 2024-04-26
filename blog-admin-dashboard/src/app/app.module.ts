a17b9767c985f5551427b0688ded556b3e43208c
MAVKT Muramudali <136581189+VilharaMuramudali@users.noreply.github.com>
2024-04-26 21:33:55 +0530

    create admin dashboard

---
+++ blog-admin-dashboard/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AngularFireModule } from '@angular/fire';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ blog-admin-dashboard/src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production:true,
+    firebaseConfig :{
+        apiKey: "AIzaSyAdleT9pYn6OWxdgm0fo_MGC6Gfw_-9t6w",
+        authDomain: "blog-application-fbac6.firebaseapp.com",
+        projectId: "blog-application-fbac6",
+        storageBucket: "blog-application-fbac6.appspot.com",
+        messagingSenderId: "303231892603",
+        appId: "1:303231892603:web:964601a83ff9e859d5b6de",
+        measurementId: "G-LQES04GS23"
+      }
+};
